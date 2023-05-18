6de354191ae3a227293c468da48edfe3d2826fce
evarkitecta <evarkitecta@gmail.com>
2023-05-18 19:37:42 +0200

    Inicio: Creación plantilla proyecto Bootstrap

---
+++ js/main.js
@@ -0,0 +1,9 @@
+//========================================================
+//Import bootstrap bundle 
+//This includes Popper and all Bootstrap's JS plugins.
+
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.js"
+
+// =======================================================
+//CUSTOM JS
+// =======================================================
